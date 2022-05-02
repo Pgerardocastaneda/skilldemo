839530b4de9221bab840c4a718750fb0057d4b76
Pgerardocastaneda <68565812+Pgerardocastaneda@users.noreply.github.com>
2022-05-01 20:46:12 -0700

    Made Test file

---
+++ skilldemotest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class skilldemotest {
+
+    @Test
+    public void subtract(){
+        assertEquals(0, skilldemo.subtract(3, 2));
+    }
+    
+}
